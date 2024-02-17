99186894bf1a7b0978a644be4915a1c49543e260
Fernando <33039864+tuxslack@users.noreply.github.com>
2024-02-17 01:33:39 -0300

    Add files via upload

--- teste.sh
+++ teste.sh
@@ -2714,19 +2714,26 @@ done
 
 
 # Verificar se o Yad, o xfce4-terminal e o xterm estão instalado no sistema.
-
+#
 # which yad              1> /dev/null 2> /dev/null || exit
-
+#
 # which xfce4-terminal   1> /dev/null 2> /dev/null || exit
-
+#
 # which xterm            1> /dev/null 2> /dev/null || exit
 
 
 
-# Termina o comprimento e a altura da tela do xfce4-terminal
+# Determina o comprimento e a altura da tela do xfce4-terminal:
 
 altura="50"
 comprimento="150"
+
+
+clear
+
+# Chamar a função verificar
+
+verificar
 
 
 
